6f7f04f6210f52fa01fdd8789a7d2de25a43967b
Allen <allenvs@mail.ru>
2020-09-21 22:00:37 +0300

    added spec and initial code

---
+++ app.js
@@ -0,0 +1,15 @@
+// Код, доставшийся вам в «наследство»
+
+// В качестве входных данных у вас будет массив с весами молекул. 
+// В качестве выходных данных необходимо вернуть число, которое обозначает вес последней молекулы. 
+// Если молекул не останется, то необходимо вернуть 0.
+var findLatestWeight = function(weights, i = weights.length - 1) {  
+    const cur = weights.length - 1 === i;  
+    
+    if (i === 0) return weights[0];  
+    
+    weights.sort((a, b) => a - b);  
+    weights[i - 1] = (weights[i] === weights[i-1]) ? 0 : weights[i] - weights[i-1];  
+    
+    return findLatestWeight(weights, i - 1);  
+}
