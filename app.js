b705c369c3abe2ff3ede3a522c6bacf25ae49041
Allen <allenvs@mail.ru>
2020-09-24 00:24:26 +0300

    added 3 subfunctions

--- app.js
+++ app.js
@@ -4,62 +4,80 @@
 // В качестве выходных данных необходимо вернуть число, которое обозначает вес последней молекулы. 
 // Если молекул не останется, то необходимо вернуть 0.
 
-var findLatestWeightFast = function(weights) {
-    // distribute the array into another grouping array
-    let weightsArray = [];
-    weights.forEach(element => {
-        weightsArray[element] = (!weightsArray[element]) ? 1 : weightsArray[element]+1;
+// group weights to indices of another array
+let groupWeights = function(array) {
+    let groupedWeights = [];
+    array.forEach(element => {
+        groupedWeights[element] = (!groupedWeights[element]) ? 1 : groupedWeights[element]+1;
     });
-    // console.table(weightsArray);
+    return groupedWeights;
+}
 
-    while (weightsArray.length > 0) {   
-        console.log('array length = ', weightsArray.length);
-        
-        let endMass = weightsArray.length-1;
-        // check if end mass as an even number or empty - annihilate those molecules/get rid of the slot
-        while (weightsArray[endMass]%2===0 || !weightsArray[endMass]) {
-            weightsArray.pop();
-            endMass = weightsArray.length-1;
-        };
-        
-        let i = weightsArray.length-1;
-        let prevOdd = false;
-        let prevIndex = 0;
-        let currentLength = weightsArray.length;
-        
-        // collision search
-        
-        // console.log("i =", i)
-        while (i >= 0) { //find too biggest masses
-            // collision happens here
-            if (weightsArray[i]) { // if there is molecules of that mass
-                // console.log(i, weightsArray[i]);
-                if (prevOdd) { // if previous mass is still there
-                    let diff = prevIndex - i; // collide the masses and get the difference mass
-                    console.log(`colliding ${prevIndex} and ${i} gives ${diff}.`)
-                    weightsArray[diff] = (!weightsArray[diff]) ? 1 : weightsArray[diff]+1; // add diff mass
-                    weightsArray[i] -= 1; // remove one smaller (current) molecule
-                    weightsArray.pop(); // annihilate those biggest molecules
-                    break;
-                };
+// cut tail till odd: clean empty / collide even
+let cutTail = function (array) {
+    while (array.length > 0 && (array[array.length-1]%2===0 || !array[array.length-1])) {
+        array.pop();
+        // console.log('pop');
+        // console.log('array length = ', groupedWeights.length);
+    };
+    return array; // -> returns tailed till odd array or empty array
+};
 
-                if (!weightsArray[i]%2===0){ // if their number is odd
-                    prevOdd = true; // previous mass didn't annihilate itself completly 
-                    prevIndex = i; // remember the mass of previous molecule
-                } else {
-                    prevOdd = false;
-                };
+// collision search of odd with first lighter molecule
+let collide = function (groupedWeights) {
+    let i = groupedWeights.length-1;
+    let prevOdd = false;
+    let prevOddIndex = 0;
+    
+    while (i >= 0) { //find too biggest masses
+        // collision happens here
+        if (groupedWeights[i]) { // if there is molecules of that mass
+            // console.log(i, groupedWeights[i]);
+            if (prevOdd) { // if previous mass is still there
+                let diff = prevOddIndex - i; // collide the masses and get the difference mass
+                console.log(`colliding ${prevOddIndex} and ${i} gives ${diff}.`)
+                groupedWeights[diff] = (!groupedWeights[diff]) ? 1 : groupedWeights[diff]+1; // add diff mass
+                groupedWeights[i] -= 1; // remove one smaller (current) molecule
+                groupedWeights.pop(); // annihilate those biggest molecules
+                break;
             };
-            i -=1;
+
+            if (!groupedWeights[i]%2===0){ // if their number is odd
+                prevOdd = true; // previous mass didn't annihilate itself completly 
+                prevOddIndex = i; // remember the mass of previous molecule
+            } else {
+                prevOdd = false;
+            };
         };
-        console.table(weightsArray);
-        //if no collision found
-        if (currentLength === weightsArray.length) return prevIndex;
+        i -=1;
+    };
+    return (groupedWeights);
+}
+
+let findLatestWeightFast = function(weights) {
+    let groupedWeights = groupWeights(weights);
+    console.table(groupedWeights);
+    // try to find last weight by colliding different molecules
+    
+    while (groupedWeights.length > 0) {   
+        groupedWeights = cutTail(groupedWeights);
+
+        let currentLength = groupedWeights.length;
+        if (currentLength === 0) return 0;
+
+        // collision search
+        groupedWeights = collide(groupedWeights);
+
+        console.table(groupedWeights);
+        
+        //if no collision happened (no second mass to collide with)
+        if (currentLength === groupedWeights.length) return groupedWeights.length-1;
 
     };
+
+    // if all molecules annihilated in collision return zero
     return 0;
 };
 
-
-console.log(findLatestWeightFast([2,7,4,1,8,10]));
-// console.log(findLatestWeightFast([2,7,4,1,8,1,1,2,3,1,4,5,6,7,8,9,3,4,5,3,4,2,5]));
+// console.log(findLatestWeightFast([2,7,4,1,8,1,3]));
+console.log(findLatestWeightFast([2,7,4,1,8,1,1,2,3,1,4,5,6,7,8,9,3,4,5,3,4,2,5,20]));
