3e5f32d2ed0ea9a6ff62170e1c2a4ad024edb95b
Allen <allenvs@mail.ru>
2020-09-25 22:17:13 +0300

    grouping done, working on colliding

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 // pseudo code:
-// group weights
-// while (length)    
-        // grab length
-        // grab last
-        // if last is even or empty - pop
-        // else odd - collide
-        // if length is same - return last index
-// return 0 
+// group weights in to a map object
+// while (map size)    
+        // grab map size
+        // find max key
+        // if max key value is even - pop
+        // else (odd) - collide -> map(max group annihilated)
+        // if map size is same - return key
+// return 0
 
 // group weights by indices of another array
 let groupWeights = function(weights) {
-    let groupedWeights = [];
+    let weightsMap = new Map();
     weights.forEach(element => {
-        groupedWeights[element] = (!groupedWeights[element]) ? 1 : groupedWeights[element]+1;
+        weightsMap.set(element, (weightsMap.get(element) || 0)+1);
     });
-    return groupedWeights;
+    console.log(Math.max(...weightsMap.keys()));
+    return weightsMap;
 };
-
+// console.log(findLatestWeightFast([2,7,4,1,8,100000000]));
+console.log(groupWeights([2,7,4,1,8,1,1,2,3,1,4,5,6,7,8,9,9,3,4,5,3,4,2,5,20]));
+// console.log()
 // collision - search smaller molecule
 let collide = function (groupedWeights) {
     let length = groupedWeights.length;
@@ -34,22 +37,30 @@ let collide = function (groupedWeights) {
 };
 
 let findLatestWeightFast = function(weights) {
-    let groupedWeights = groupWeights(weights);
+    let weightsMap = weightsMap(weights);
     
-    while (groupedWeights.length) {       
-        let length = groupedWeights.length;
-        let last = groupedWeights[length-1];
-
-        if (last%2===0 || !last) { // even or empty
-            groupedWeights.pop();
-        } else { // odd
-            groupedWeights = collide(groupedWeights);
+    while (weightsMap.size) {       
+        let size = weightsMap.size; // grab map size
+        let max = Math.max(...weightsMap.keys()); // find max key
+        
+        if (weightsMap.get(max)%2===0) { // if max value is even
+            weightsMap.delete(max); // pop max
+        } else { // if max value is odd - collide
+            weightsMap.delete(max); // pop max
+            let nextMax = Math.max(...weightsMap.keys()); // find next to max
+            weightsMap.set(nextMax, weightsMap.get(nextMax)-1) // remove one molecule
         };
-        if (length === groupedWeights.length) return length-1; // no collision happened
+
+        break;
+        // if (last%2===0 || !last) { // even or empty
+        //     groupedWeights.pop();
+        // } else { // odd
+        //     groupedWeights = collide(groupedWeights);
+        // };
+        // if (length === groupedWeights.length) return length-1; // no collision happened
     };
     // if all molecules annihilated in collision return zero
     return 0;
 };
 
-console.log(findLatestWeightFast([2,7,4,1,8,1]));
-// console.log(findLatestWeightFast([2,7,4,1,8,1,1,2,3,1,4,5,6,7,8,9,9,3,4,5,3,4,2,5,20]));
+
