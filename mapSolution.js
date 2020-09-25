1eb26436bee818c8de7d592158995154bb77e7d8
Allen <allenvs@mail.ru>
2020-09-26 00:48:01 +0300

    found a bug need to fix the map solution - wrong anwer

--- mapSolution.js
+++ mapSolution.js
@@ -8,7 +8,7 @@
         // if map size is same - return key
 // return 0
 
-// group weights by indices of another array
+// group weights to map
 let mapWeights = function(weights) {
     let weightsMap = new Map();
     weights.forEach(element => {
@@ -33,7 +33,9 @@ let collide = function (weightsMap) {
             weightsMap.set(nextMax, weightsMap.get(nextMax)-1) // remove one molecule
             if (weightsMap.get(nextMax)===0) weightsMap.delete(nextMax); // if next is empty - pop
             weightsMap.set(max-nextMax, (weightsMap.get(max-nextMax) || 0)+1); // add diff mass molecule
-            // console.log(`colliding ${max} and ${nextMax} creates ${max-nextMax}`);
+            console.log(`colliding ${max} and ${nextMax} creates ${max-nextMax}`);
+            console.log(weightsMap);
+            // max = (max-nextMax>nextMax)? max-nextMax : nextMax;
         };
     };
     return (weightsMap);
@@ -51,6 +53,7 @@ let findLatestWeight = function(weights) {
     return 0;
 };
 
-console.log(findLatestWeight([2,7,4,1,8,1]));
-// console.log(findLatestWeight([2,7,4,1,8,1,1,2,3,1,4,5,6,7,8,9,9,3,4,5,3,4,2,5,2,1,1]));
-// console.log(findLatestWeight([2,2,2,2,1,1,3,3,4,4,8,8,]))
+// console.log(findLatestWeight([2,7,4,1,8,1])===1); //
+// console.log(findLatestWeight([2,7,4,1,8,1,1,2,3,1,4,5,6,7,8,9,9,3,4,5,3,4,2,5,2,1,1])===0);
+console.log(findLatestWeight([10,5,5,1,10000,50000,33333]));
+
